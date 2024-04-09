4fb5882e6f600c84b8b83039d534e6e329efa9a6
jflores353 <jflores353@gmail.com>
2024-04-09 10:23:20 -0500

    Updating layout for tablet and desktop

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const mobileNav = document.getElementById("mobile-nav");
 const menuBtn = document.getElementById("menu-btn");
 const closeBtn = document.getElementById("close-btn");
+const closeMenu = document.querySelectorAll(".close-menu");
 
 menuBtn.addEventListener("click", () => {
 	mobileNav.style.display = "flex";
@@ -9,3 +10,21 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
 	mobileNav.style.display = "none";
 });
+
+closeMenu.forEach((item) => {
+	item.addEventListener("click", () => {
+		mobileNav.style.display = "none";
+	});
+});
+
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("navbar").style.top = "0";
+	} else {
+		document.getElementById("navbar").style.top = "-75px";
+	}
+}
